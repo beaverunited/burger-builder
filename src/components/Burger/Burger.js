7a24a4d384d20a37661167272ceade2bf21127d7
Michael <michaelsbritton@gmail.com>
2020-10-10 23:47:34 +0900

    try 2

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            }); // [x,y]   Transforms and object of key/value pairs to an array of burger ingredients... I don't think I could have thought of the above formula on my own...
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>You may now start adding ingredients</p>
+    }
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default burger;
---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Auxilliary from '../../hoc/Auxilliary';
+import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render () {
+        return (
+            <Auxilliary>
+                <Toolbar />
+                    <SideDrawer 
+                        open={this.state.showSideDrawer} 
+                        closed={this.sideDrawerClosedHandler}
+                        />
+                        <main className={classes.Content}>
+                            {this.props.children}
+                        </main>
+            </Auxilliary>
+        )
+    }
+}
+
+export default Layout;
